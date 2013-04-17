f2c3254c286ec62c8f1e43b11a3531551a280d09
Christopher Cassano <chris@h4ckb00k.local>
2013-04-16 20:13:57 -0400

    Initial Commit

---
+++ SendHubTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SendHubTest
+//
+//  Created by Christopher Cassano on 4/16/13.
+//  Copyright (c) 2013 ChrisVCo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SendHubTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SendHubTest
+//
+//  Created by Christopher Cassano on 4/16/13.
+//  Copyright (c) 2013 ChrisVCo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
